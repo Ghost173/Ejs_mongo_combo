c84b52e5a714d5564c81e38cc19c557cb8b1fd37
ghost173 <jpirakas007@gmail.com>
2022-12-13 22:08:53 +0530

    Mongo DB connection

---
+++ server/database/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const connectDB = async () =>{
+    try {
+        //mongo connection
+        const con =  await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
+
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
